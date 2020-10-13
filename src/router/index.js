4483219ad9c874c556c68762fc34225e1bf03610
irifune-tatsuya <irifune.tatsuya@gmail.com>
2020-10-13 19:02:50 +0900

    add_routing

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,29 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/career',
+    name: 'career',
+    component: () => import('../views/Career.vue')
+  },
+  {
+    path: '/accounts',
+    name: 'accounts',
+    component: () => import('../views/Accounts.vue')
+  },
+  {
+    path: '/commentary',
+    name: 'commentary',
+    component: () => import('../views/commentary.vue')
+  },
+  {
+    path: '/demo',
+    name: 'demo',
+    component: () => import('../views/demo.vue')
   }
 ]
 
