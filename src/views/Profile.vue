a3eedeb88c6a4553324a916b713fa7874e96021f
irifune-tatsuya <irifune.tatsuya@gmail.com>
2020-10-17 22:14:12 +0900

    use_Vuex

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,7 +4,7 @@
     <h1 class="title">profile</h1>
     <div class="profile__contents">
       <ul class="profile__contents__list">
-        <li class="profile__contents__list__comment" v-for="(profile) in upperProfiles" :key="profile.id"> 
+        <li class="profile__contents__list__comment" v-for="profile in $store.state.upperProfiles" :key="profile.id"> 
           <div class="profile__contents__list__comment__white" v-if="profile.value" @click="profile.value = !profile.value">{{ profile.text1 }}</div>
           <div class="profile__contents__list__comment__gray2" v-else @click="profile.value = !profile.value">{{ profile.text2 }}</div>
           <div class="profile__contents__list__comment__gray" v-if="profile.value"></div>
@@ -13,7 +13,7 @@
       </ul>
       <div class="profile__contents--mustacheman"></div>
       <ul class="profile__contents__list">
-        <li class="profile__contents__list__comment" v-for="profile in rowerProfiles" :key="profile.id"> 
+        <li class="profile__contents__list__comment" v-for="profile in $store.state.rowerProfiles" :key="profile.id"> 
           <div class="profile__contents__list__comment__white" v-if="profile.value" @click="profile.value = !profile.value">{{ profile.text1 }}</div>
           <div class="profile__contents__list__comment__gray2" v-else @click="profile.value = !profile.value">{{ profile.text2 }}</div>
           <div class="profile__contents__list__comment__gray" v-if="profile.value"></div>
@@ -33,20 +33,6 @@ export default {
   components: {
     Header,
     Footer
-  },
-  data: function() {
-    return {
-      upperProfiles: [
-        {id: 1, value: "true", text1: "名古屋に住んでいます", text2: "もとは福岡に住んでいました"},
-        {id: 2, value: "true", text1: "PHP/HTML/CSSとか", text2: "Ruby/Railsはうろ覚え"},
-        {id: 3, value: "true", text1: "仮面ライダーちょい好き芸人", text2: "ここ２年見れてません泣"}
-      ],
-      rowerProfiles: [
-        {id: 1, value: "true", text1: "自社サイトの運用保守", text2: "開発職に憧れています!!"},
-        {id: 2, value: "true", text1: "もくもく会したい!!", text2: "本当は無口で人見知り"},
-        {id: 3, value: "true", text1: "Twitterで時々つぶやく", text2: "いいね&フォローお願い!!"}
-      ]
-    }
   }
 }
 </script>
