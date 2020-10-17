b9258866806883d864781ac52aaa44f507a5bbc7
irifune-tatsuya <irifune.tatsuya@gmail.com>
2020-10-17 21:04:05 +0900

    add_account_page

--- src/components/ProfileList.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <li class="profile__contents__list__comment profile__contents__list__comment--right">
-    <div class="profile__contents__list__comment__white" v-if="profile1" @click="profile1 = !profile1"><slot></slot></div>
-    <div class="profile__contents__list__comment__gray2" v-else @click="profile1 = !profile1">もとは福岡に住んでいました</div>
-    <div class="profile__contents__list__comment__gray" v-if="profile1"></div>
-    <div class="profile__contents__list__comment__white2" v-else></div>
-  </li>
-</template>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,7 +3,7 @@
     <h1 class="title">profile</h1>
     <div class="profile__contents">
       <ul class="profile__contents__list">
-        <li class="profile__contents__list__comment" v-for="profile in upperProfile" :key="profile.label"> 
+        <li class="profile__contents__list__comment" v-for="(profile) in upperProfiles" :key="profile.id"> 
           <div class="profile__contents__list__comment__white" v-if="profile.value" @click="profile.value = !profile.value">{{ profile.text1 }}</div>
           <div class="profile__contents__list__comment__gray2" v-else @click="profile.value = !profile.value">{{ profile.text2 }}</div>
           <div class="profile__contents__list__comment__gray" v-if="profile.value"></div>
@@ -12,7 +12,7 @@
       </ul>
       <div class="profile__contents--mustacheman"></div>
       <ul class="profile__contents__list">
-        <li class="profile__contents__list__comment" v-for="profile in rowerProfile" :key="profile.label"> 
+        <li class="profile__contents__list__comment" v-for="profile in rowerProfiles" :key="profile.id"> 
           <div class="profile__contents__list__comment__white" v-if="profile.value" @click="profile.value = !profile.value">{{ profile.text1 }}</div>
           <div class="profile__contents__list__comment__gray2" v-else @click="profile.value = !profile.value">{{ profile.text2 }}</div>
           <div class="profile__contents__list__comment__gray" v-if="profile.value"></div>
@@ -27,15 +27,15 @@ export default {
   name: 'Profile',
   data: function() {
     return {
-      upperProfile: [
-        {label: "A", value: "true", text1: "名古屋に住んでいます", text2: "もとは福岡に住んでいました"},
-        {label: "B", value: "true", text1: "PHP/HTML/CSSとか", text2: "Ruby/Railsはうろ覚え"},
-        {label: "C", value: "true", text1: "仮面ライダーちょい好き芸人", text2: "ここ２年見れてません泣"}
+      upperProfiles: [
+        {id: 1, value: "true", text1: "名古屋に住んでいます", text2: "もとは福岡に住んでいました"},
+        {id: 2, value: "true", text1: "PHP/HTML/CSSとか", text2: "Ruby/Railsはうろ覚え"},
+        {id: 3, value: "true", text1: "仮面ライダーちょい好き芸人", text2: "ここ２年見れてません泣"}
       ],
-      rowerProfile: [
-        {label: "A", value: "true", text1: "自社サイトの運用保守", text2: "開発職に憧れています!!"},
-        {label: "B", value: "true", text1: "もくもく会したい!!", text2: "本当は無口で人見知り"},
-        {label: "C", value: "true", text1: "Twitterで時々つぶやく", text2: "いいね&フォローお願い!!"}
+      rowerProfiles: [
+        {id: 1, value: "true", text1: "自社サイトの運用保守", text2: "開発職に憧れています!!"},
+        {id: 2, value: "true", text1: "もくもく会したい!!", text2: "本当は無口で人見知り"},
+        {id: 3, value: "true", text1: "Twitterで時々つぶやく", text2: "いいね&フォローお願い!!"}
       ]
     }
   }
