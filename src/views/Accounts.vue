167658a2539f1d49d2adc5d251a218b84eedef77
irifune-tatsuya <irifune.tatsuya@gmail.com>
2020-10-18 22:42:33 +0900

    edit_CSS

--- src/views/Accounts.vue
+++ src/views/Accounts.vue
@@ -4,14 +4,16 @@
     <div class="accounts">
       <h1 class="title">accounts</h1>
       <div class="accounts__contents">
-        <a class="twitter-timeline" data-lang="ja" data-width="35vw" data-height="30vw" :data-theme="darkMode()" href="https://twitter.com/iczo32"></a>
-        <div class="accounts__contents__list">
-          <a href="https://twitter.com/iczo32"><div class="accounts__contents__list__links twitter"><span class="fab fa-twitter"></span>twitter account</div></a>
-          <a href="https://qiita.com/iczo32"><div class="accounts__contents__list__links qiita">Qiita account</div></a>
-          <a href="https://github.com/irifune-tatsuya"><div class="accounts__contents__list__links github"><span class="fab fa-github"></span>GitHub account</div></a>
-          <div class="accounts__contents__list__message">
-            <div class="accounts__contents__list__message--text"><p>Please Follow me !!</p></div>
-            <div class="accounts__contents__list__message--img"></div>
+        <div class="accounts__contents__right">
+          <a class="twitter-timeline" data-lang="ja" data-width="35vw" data-height="30vw" :data-theme="darkMode()" href="https://twitter.com/iczo32"></a>
+        </div>
+        <div class="accounts__contents__left">
+          <a href="https://twitter.com/iczo32"><div class="accounts__contents__left__links twitter"><span class="fab fa-twitter"></span>twitter account</div></a>
+          <a href="https://qiita.com/iczo32"><div class="accounts__contents__left__links qiita">Qiita account</div></a>
+          <a href="https://github.com/irifune-tatsuya"><div class="accounts__contents__left__links github"><span class="fab fa-github"></span>GitHub account</div></a>
+          <div class="accounts__contents__left__message">
+            <div class="accounts__contents__left__message--text"><p>Please Follow me !!</p></div>
+            <div class="accounts__contents__left__message--img"></div>
           </div>
         </div>
       </div>
